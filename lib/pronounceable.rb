89cfb9de50fe5739b1e55552f4777f534f4dd325
ecin <ecin@copypastel.com>
2010-12-22 20:36:50 -0400

    Remove use of Regexp's in favor of a (very) pseudo state machine. Performs roughly 3x better.

--- lib/pronounceable.rb
+++ lib/pronounceable.rb
@@ -1,16 +1,27 @@
 module Pronounceable
-  @@acceptable = /a|e|i|o|u/
-
-  consecutive_letters = ('a'..'z').inject([]) do |array, letter|
-    array << letter * 2
-  end - ['ee', 'oo']
-  consecutive_consonants = ('a'..'z').to_a.-(['a', 'e', 'i', 'o', 'u']).repeated_permutation(3).map(&:join)
-  consecutive_vowels = ['a', 'e', 'i', 'o', 'u'].repeated_permutation(3).map(&:join)
-  @@unacceptable = Regexp.new((consecutive_letters + consecutive_consonants + consecutive_vowels).join('|'))
+  @@vowels = ['a', 'e', 'i', 'o', 'u']
 
   def pronounceable?
-    !!self.match(@@acceptable) and
-    !self.match(@@unacceptable)
+    vowel_found     = false
+    current_vowel   = nil
+    prev_vowel      = nil
+    prev_prev_vowel = nil
+    prev_letter     = nil
+
+    self.each_char do |char|
+      if @@vowels.include?(char)
+        vowel_found   = true
+        current_vowel = true
+      else
+        current_vowel = false
+      end
+      return false if char == prev_letter unless char == 'e' or char == 'o'
+      return false if prev_prev_vowel == prev_vowel and prev_vowel == current_vowel
+      prev_prev_vowel = prev_vowel
+      prev_vowel = current_vowel
+      prev_letter = char
+    end
+    vowel_found and true
   end
 
 end
